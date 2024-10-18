db6340b9ae8d73badf284de952cbbcd2993cef62
Saqib-DevSecOp <saqibahmad778866@gmail.com>
2024-10-19 00:32:46 +0500

    WEBSITE : website fully implemented

--- src/web/website/urls.py
+++ src/web/website/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import (
-    HomeView
+    HomeView, ServiceView, AboutUsView, ContactUsView, BlogListView, BlogDetailView, PrivacyPolicyView,
+    TermsAndConditionView
 )
 
 app_name = "website"
 urlpatterns = [
-    #path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('services/', ServiceView.as_view(), name='services'),
+    path('about-us/', AboutUsView.as_view(), name='about'),
+    path('become-a-provider/', TemplateView.as_view(template_name='website/become_a_provider.html'), name='become-a-provider'),
+    path('blog-list/', BlogListView.as_view(), name='blog-list'),
+
+    path('blog-detail/', BlogDetailView.as_view(), name='blog-detail'),
+    path('contact-us/', ContactUsView.as_view(), name='contact-us'),
+
+    path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy-policy'),
+    path('terns-and-condition/', TermsAndConditionView.as_view(), name='terms-and-condition'),
 ]
--- src/web/website/views.py
+++ src/web/website/views.py
@@ -7,3 +7,31 @@ from src.apps.whisper.main import NotificationService
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'website/index.html'
+
+
+class ServiceView(TemplateView):
+    template_name = 'website/services.html'
+
+
+class AboutUsView(TemplateView):
+    template_name = 'website/about_us.html'
+
+
+class BlogListView(TemplateView):
+    template_name = 'website/blog_list.html'
+
+
+class BlogDetailView(TemplateView):
+    template_name = 'website/blog_detail.html'
+
+
+class ContactUsView(TemplateView):
+    template_name = 'website/contact_us.html'
+
+
+class PrivacyPolicyView(TemplateView):
+    template_name = 'website/privacy_policy.html'
+
+
+class TermsAndConditionView(TemplateView):
+    template_name = 'website/terms_and_condition.html'
