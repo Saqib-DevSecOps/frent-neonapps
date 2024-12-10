43cf43ca2ba3e8b92cf420f2ce3faeae70bcba46
hesham <hesham.fractured@gmail.com>
2024-12-11 04:24:34 +0500

    UPDT: Service Detail Page

--- src/services/services/urls.py
+++ src/services/services/urls.py
@@ -7,4 +7,6 @@ app_name = "services"
 urlpatterns = [
     path('services/api/', include('src.services.services.api.urls', namespace='services-api')),
     path('service/', ServicesListView.as_view(), name='services-list'),
+    path('service/<str:pk>/', ServiceDetailView.as_view(), name='services-detail'),
+
 ]
--- src/services/services/views.py
+++ src/services/services/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from src.services.services.filters import ServiceFilter
-from src.services.services.models import Service
+from src.services.services.models import Service, ServiceAvailability
 
 
 class ServicesListView(ListView):
@@ -21,7 +21,10 @@ class ServicesListView(ListView):
 
 class ServiceDetailView(DetailView):
     model = Service
-    template_name = ''
 
-    def get_object(self, queryset=None):
-        return get_object_or_404(Service, pk=self.kwargs['pk'])
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        service = self.object
+        availability_slots = ServiceAvailability.objects.filter(service=service, is_active=True).order_by('day_of_week', 'start_time')
+        context['availability_slots'] = availability_slots
+        return context
