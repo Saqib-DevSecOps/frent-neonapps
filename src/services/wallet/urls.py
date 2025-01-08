ded94ef64891e5bd14bd696011a853a67387f3b1
hesham <hesham.fractured@gmail.com>
2025-01-08 19:06:18 +0500

    FIX: Pagination Added.

--- src/services/wallet/urls.py
+++ src/services/wallet/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from src.services.wallet.views import UserWalletView, TransactionListView
+from src.services.wallet.views import UserWalletView, TransactionListView, UserTransactionListView
 
 app_name = "wallet"
 urlpatterns = [
     path('user-wallet/<int:pk>/', UserWalletView.as_view(), name='user-wallet'),
     path('wallet/list/', TransactionListView.as_view(), name='wallet-list'),
+    path('user-transaction/<int:pk>/', UserTransactionListView.as_view(), name='user-transaction'),
 
 ]
--- src/services/wallet/views.py
+++ src/services/wallet/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView
 
+from src.services.users.models import User
 from src.services.wallet.models import Wallet, Transaction
 
 
@@ -17,3 +18,20 @@ class UserWalletView(DetailView):
 class TransactionListView(ListView):
     model = Transaction
     template_name = 'wallet/transaction_list.html'
+    paginate_by = 20
+
+
+class UserTransactionListView(ListView):
+    model = Transaction
+    template_name = 'wallet/user_transaction.html'
+    context_object_name = 'object_list'
+    paginate_by = 20
+
+    def get_queryset(self):
+        user = get_object_or_404(User, pk=self.kwargs['pk'])
+        return Transaction.objects.filter(user=user)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserTransactionListView, self).get_context_data(**kwargs)
+        context['wallet_user'] = get_object_or_404(User, pk=self.kwargs['pk'])
+        return context
