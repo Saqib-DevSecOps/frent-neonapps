567ebfbea257252ec4cde56f2629db18149ce43d
hesham <hesham.fractured@gmail.com>
2025-01-08 18:29:15 +0500

    UPDT: Transactins & Wallet Pages

--- src/services/wallet/urls.py
+++ src/services/wallet/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from src.services.wallet.views import UserWalletView, WalletListView
+from src.services.wallet.views import UserWalletView, TransactionListView
 
 app_name = "wallet"
 urlpatterns = [
     path('user-wallet/<int:pk>/', UserWalletView.as_view(), name='user-wallet'),
-    path('wallet/list/', WalletListView.as_view(), name='wallet-list'),
+    path('wallet/list/', TransactionListView.as_view(), name='wallet-list'),
 
 ]
--- src/services/wallet/views.py
+++ src/services/wallet/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
-from src.services.wallet.models import Wallet
+from src.services.wallet.models import Wallet, Transaction
 
 
 # Create your views here.
@@ -9,6 +9,11 @@ class UserWalletView(DetailView):
     model = Wallet
     template_name = 'wallet/user_wallet.html'
 
-class WalletListView(ListView):
-    model = Wallet
-    template_name = 'wallet/wallet_list.html'
+    def get_context_data(self, **kwargs):
+        context = super(UserWalletView, self).get_context_data(**kwargs)
+        context['object_list'] = Transaction.objects.filter(wallet=self.object)
+        return context
+
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = 'wallet/transaction_list.html'
