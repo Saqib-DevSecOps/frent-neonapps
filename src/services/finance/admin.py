ef16b465f8dd8c957efd2feae15ae48a4a911b7a
Saqib-DevSecOp <saqibahmad778866@gmail.com>
2025-02-03 11:46:38 +0500

    Feat: FInance App Added With New models

---
+++ src/services/finance/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from .models import (
+    BankAccount,
+    PayPalAccount,
+    Withdrawal
+)
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'user', 'account_holder_name', 'bank_name', 'iban', 'currency', 'status', 'is_active', 'created_on'
+    ]
+
+
+@admin.register(PayPalAccount)
+class PayPalAccountAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'user', 'account_holder_name', 'email', 'status', 'is_active', 'created_on'
+    ]
+
+
+@admin.register(Withdrawal)
+class WithdrawalAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'user', 'amount', 'status', 'created_on'
+    ]
--- src/services/wallet/models.py
+++ src/services/wallet/models.py
@@ -1,7 +1,6 @@
 import uuid
 
 from cities_light.models import Country
-from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.db import models
